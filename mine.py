75894dc75c6112a74f7a6071648eebfa01d774d5
alebalcazarr <157438503+alebalcazarr@users.noreply.github.com>
2024-01-22 23:45:18 -0500

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,7 +1,7 @@
-# Write a line of code that prints "Hello world!".
-
-
 def hello_world():
-  # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
-  
-  return variable_name
+    # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
+    variable_name = "Hello world!"
+    return variable_name
+
+# Print the result of the hello_world function
+print(hello_world())
